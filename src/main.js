84c0da99c801a19faa3673a1984e41cefb4fa067
Rabia <rabiahaucha9@gmail.com>
2024-04-17 15:17:35 +0200

    FileStructure

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@/assets/style.css'
 
 Vue.config.productionTip = false
 
